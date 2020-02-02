c7e59268ae919a0e34f93bf4cbb879400a4e376b
spectrosdennoce <spectrosdennoce@gmail.fr>
2020-02-02 23:10:36 +0100

    amelie half register formulaire

--- src/Controller/Menu.php
+++ src/Controller/Menu.php
@@ -5,6 +5,7 @@ use App\Entity\Utils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class Menu extends AbstractController
 {
     public function index()
@@ -15,8 +16,30 @@ class Menu extends AbstractController
     {
         return $this->render('Login.html.twig');
     }
-    public function Register()
+    public function Register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        // 1) contruit le formulaire 
+        $user = new Utils();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) submit se produit au "post"
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode le mdp 
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // Enregistre le mot de passe 
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
+            // set un "flash" sucess pour l'utilisateur et son inscription
+
+            return $this->redirectToRoute('user_registration');
+        }
         return $this->render('Register.html.twig');
     }
     public function Add_Formulaire()
