528121990138eebb2239efe91aa208c73afefd60
spectrosdennoce <spectrosdennoce@gmail.fr>
2020-02-03 14:23:24 +0100

    vendor dev gitignore

--- var/cache/dev/twig/26/265e39126b684fd791a02dfc33ca55d35aaa57332d8855939c3c7f2c6e92f90f.php
+++ var/cache/dev/twig/26/265e39126b684fd791a02dfc33ca55d35aaa57332d8855939c3c7f2c6e92f90f.php
@@ -28,6 +28,8 @@ class __TwigTemplate_19d844ae7da8de417993f042407cfa4fb80ae980e9d2b21331c6f877cad
 
         $this->blocks = [
             'title' => [$this, 'block_title'],
+            'javascripts' => [$this, 'block_javascripts'],
+            'head' => [$this, 'block_head'],
             'body' => [$this, 'block_body'],
         ];
     }
@@ -54,11 +56,18 @@ class __TwigTemplate_19d844ae7da8de417993f042407cfa4fb80ae980e9d2b21331c6f877cad
       <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">
       <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
     </head>
-    <body>
+    ";
+        // line 10
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 35
+        echo "    <body>
       ";
-        // line 11
+        // line 36
+        $this->displayBlock('head', $context, $blocks);
+        // line 39
+        echo "      ";
         $this->displayBlock('body', $context, $blocks);
-        // line 12
+        // line 40
         echo "    </body>
 </html>
 ";
@@ -89,7 +98,73 @@ class __TwigTemplate_19d844ae7da8de417993f042407cfa4fb80ae980e9d2b21331c6f877cad
 
     }
 
-    // line 11
+    // line 10
+    public function block_javascripts($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        // line 11
+        echo "      \$('#exampleModal').on('show.bs.modal', function (event) {
+        var button = \$(event.relatedTarget)
+        var recipient = button.data('whatever')
+        var modal = \$(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+      })
+      \$('#formulaire').submit(function(event) {
+
+        event.preventDefault();
+
+        var \$form = \$(this),
+            term = \$form.find( \"input[name='text']\" ).val(),
+            url = \$form.attr( \"action\" );
+
+        var posting = \$.ajax(
+        type: \"POST\",
+        url: url,
+        data: term
+      });
+
+      });
+      });
+    ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 36
+    public function block_head($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "head"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "head"));
+
+        // line 37
+        echo "        ";
+        $this->loadTemplate("Register.html.twig", "Header-Footer.html.twig", 37)->display($context);
+        // line 38
+        echo "      ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 39
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
@@ -114,7 +189,7 @@ class __TwigTemplate_19d844ae7da8de417993f042407cfa4fb80ae980e9d2b21331c6f877cad
 
     public function getDebugInfo()
     {
-        return array (  93 => 11,  74 => 5,  62 => 12,  60 => 11,  51 => 5,  45 => 1,);
+        return array (  168 => 39,  158 => 38,  155 => 37,  145 => 36,  112 => 11,  102 => 10,  83 => 5,  71 => 40,  68 => 39,  66 => 36,  63 => 35,  61 => 10,  53 => 5,  47 => 1,);
     }
 
     public function getSourceContext()
@@ -128,7 +203,35 @@ class __TwigTemplate_19d844ae7da8de417993f042407cfa4fb80ae980e9d2b21331c6f877cad
       <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">
       <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
     </head>
+    {% block javascripts %}
+      \$('#exampleModal').on('show.bs.modal', function (event) {
+        var button = \$(event.relatedTarget)
+        var recipient = button.data('whatever')
+        var modal = \$(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+      })
+      \$('#formulaire').submit(function(event) {
+
+        event.preventDefault();
+
+        var \$form = \$(this),
+            term = \$form.find( \"input[name='text']\" ).val(),
+            url = \$form.attr( \"action\" );
+
+        var posting = \$.ajax(
+        type: \"POST\",
+        url: url,
+        data: term
+      });
+
+      });
+      });
+    {% endblock %}
     <body>
+      {% block head %}
+        {% include 'Register.html.twig' %}
+      {% endblock %}
       {% block body %}{% endblock %}
     </body>
 </html>
